d57a53e40aad3dda247ab6f481540ab66524abb9
Nicolas De La Fuente <nicoolasdelafuente@gmail.com>
2023-04-18 18:59:41 -0300

    Crew Page styles for tablet

--- src/components/ContentHeroCrew/ContentHeroCrew.js
+++ src/components/ContentHeroCrew/ContentHeroCrew.js
@@ -4,26 +4,27 @@ import imageCrew from "../../Pages/HeroCrew/crew/image-douglas-hurley.png"
 const ContentHeroCrew = () => {
 
     return <div className="wrapperCrew">
-                <h2 className="titleDestination"><span>02</span>Meet your Crew</h2>
-                <div className="imageCrew">
-                    <div className="imgCrew"></div>
+        <h2 className="titleDestination"><span>02</span>Meet your Crew</h2>
+        <div className="img-and-details-crew">
+            <div className="imageCrew">
+                <div className="imgCrew"></div>
+            </div>
+            <div className="list-and-details">
+                <ul className="optionsCrew">
+                    <li><div className="pointMenu active-back"></div></li>
+                    <li><div className="pointMenu"></div></li>
+                    <li><div className="pointMenu"></div></li>
+                    <li><div className="pointMenu"></div></li>
+                </ul>
+                <div className="details">
+                    <h2 className="jobCrew">commander</h2>
+                    <h1 className="nameCrew">douglas hurley</h1>
+                    <p className="textCrew">Douglas Gerald Hurley is an American engineer, former Marine Corps pilot
+                        and former NASA astronaut. He launched into space for the third time as
+                        commander of Crew Dragon Demo-2.</p>
                 </div>
-                <div className="list-and-details">
-                    <ul className="optionsCrew">
-                        <li><div className="pointMenu active"></div></li>
-                        <li><div className="pointMenu"></div></li>
-                        <li><div className="pointMenu"></div></li>
-                        <li><div className="pointMenu"></div></li>
-                    </ul>
-                    <div className="details">
-                        <h2 className="jobCrew">commander</h2>
-                        <h1 className="nameCrew">douglas hurley</h1>
-                        <p className="textCrew">Douglas Gerald Hurley is an American engineer, former Marine Corps pilot 
-  and former NASA astronaut. He launched into space for the third time as 
-  commander of Crew Dragon Demo-2.</p>
-                    </div>
-                </div>
-
+            </div>
+        </div>
     </div>
 }
 
